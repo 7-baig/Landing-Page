37a802572f7d1e69ec19ca686d660c905b43f93d
Saad Baig <saadbaig7777@gmail.com>
2020-08-20 17:41:34 +0500

    added Lower Footer in Footer

--- src/components/Footer/LowerFooter.jsx
+++ src/components/Footer/LowerFooter.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-import { Container } from './StyleLowerFooter'
+import { Container, Address, H5, P } from './StyleLowerFooter'
 
 export const LowerFooter = () => (
     <Container>
-        
+        <Address>
+            <H5>NEW VANTAGE JOHANNESBERG</H5>
+            <P>+2780808-8080</P>
+        </Address>
+        <Address>
+            <H5>NEW VANTAGE CAPE TOWN</H5>
+            <P>+2780808-8080</P>
+        </Address>
     </Container>
 )
--- src/components/Footer/StyleLowerFooter.js
+++ src/components/Footer/StyleLowerFooter.js
@@ -1,7 +1,34 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-    border: 1px solid #000;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    align-items: center;
+    background: #000000;
+`
+export const Address = styled.div`
     display: flex;
     flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 40px 0px;
+    @media (max-width: 480px) {
+        margin: 20px 0px;
+    }
+`
+export const H5 = styled.h5`
+    color: #ffffff;
+    margin: 0;
+    padding: 0;
+    font-size: 12px;
+`
+export const P = styled.p`
+    color: #ffffff;
+    margin: 0;
+    padding: 0;
+    font-size: 12px;
+    opacity: 0.6;
+    letter-spacing: 2px;
+
 `
