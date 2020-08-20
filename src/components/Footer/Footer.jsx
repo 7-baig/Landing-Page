a528af3ac7de18aa60a0ba14917a970823f8770e
Saad Baig <saadbaig7777@gmail.com>
2020-08-20 18:40:19 +0500

    added slash icon in footr

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import slash from '../../images/triple-slash.png'
 import { UpperFooter } from './UpperFooter'
 import { LowerFooter } from './LowerFooter'
-import { Container } from './StyleFooter'
+import { Container, Slash } from './StyleFooter'
 
 export const Footer = () => (
     <Container>
+        <Slash src={slash}/>
         <UpperFooter />
         <LowerFooter />
     </Container>
--- src/components/Footer/StyleFooter.js
+++ src/components/Footer/StyleFooter.js
@@ -1,8 +1,18 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-    border: 1px solid #000;
     display: flex;
     flex-direction: column;
     font-family: sans-serif;
+`
+export const Slash = styled.img`
+    width: 50px;
+    margin: 0;
+    padding: 0;
+    align-self: center;
+    transform: translateY(30px);
+    @media (max-width: 480px) {
+        width: 30px;
+        transform: translateY(15px);
+    };
 `
